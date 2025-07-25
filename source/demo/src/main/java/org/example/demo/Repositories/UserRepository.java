e30db7787c3c8cec424a56a2ed4c1d8784f83608
VuongDD <vuongddhe186713@fpt.edu.vn>
2025-07-25 15:09:59 +0700

    update home

--- source/demo/src/main/java/org/example/demo/Repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package org.example.demo.Repositories;
-
-
-import org.example.demo.Entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
